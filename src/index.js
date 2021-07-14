9ec54f699aba741f2d2b4801edb7d6279e11a126
unknown <jaewoo010207@gmail.com>
2021-07-14 22:46:12 +0900

    no1

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('potato')
+);
+
+// react application이 하나의 component만을 rendering 해야한다.
+// component를 작성할 때 마다 import React from "react" 써주기
+// 써주지 않으면 react는 jsx가 있는 component를 사용하는 것을 이해하지 못함!
+// 컴포넌트는 HTML을 반환하는 함수다.
+// 이런 컴포넌트들을 app에 넣어주면 된다
